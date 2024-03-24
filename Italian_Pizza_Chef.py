ef89e6f01ac26e31148f126fcd2bc0c451dbf766
stefan-b10 <stefan_budai88@yahoo.com>
2024-03-24 17:12:13 +0200

    new file:   Italian_Pizza_Chef.py modified:   main.py

---
+++ Italian_Pizza_Chef.py
@@ -0,0 +1,47 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+messages = [
+     {
+          "role": "system",
+          "content": "You are one of the best Pizza Cef in the world. You are very strict and precise with ingredients and recipes for Pizza."                   
+     }
+]
+
+model = "gpt-3.5-turbo"
+
+stream = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True,
+    )
+
+collected_messages = []
+
+while True:
+    print("\n")
+    user_input = input()
+    messages.append(
+        {
+            "role": "user",
+            "content": user_input
+        }
+    )
+    stream = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True,
+    )
+    collected_messages = []
+    for chunk in stream:
+        chunk_message = chunk.choices[0].delta.content or ""
+        print(chunk_message, end="")
+        collected_messages.append(chunk_message)
+    
+    messages.append(
+        {
+            "role": "system",
+            "content": "".join(collected_messages)
+        }
+    )
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from simple_term_menu import TerminalMenu
 
 print("Select your chef")
 
-chefs_dict = {"Old Turkish Woman Chef":"Old_Turkish_Woman_Chef.py","Bartender":"Bartender.py"}
+chefs_dict = {"Old Turkish Woman Chef":"Old_Turkish_Woman_Chef.py","Bartender":"Bartender.py","Italian Pizza Chef":"Italian_Pizza_Chef.py"}
 options = list(chefs_dict.keys())
 
 terminal_menu = TerminalMenu(options)
